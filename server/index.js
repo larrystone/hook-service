dc842aa5a85f05c299f1ee4e26e71939f26cb7f0
lawal lanre <lanre.lawal@andela.com>
2018-08-31 14:46:49 +0100

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import { json, text, send } from 'micro';
+
+const main = async (req, res) => {
+  try {
+    const body = await json(req);
+    console.log('body-----', body)
+    send(res, 200, body);
+  } catch (error) {
+    console.log('error-----', error)
+    // if (400 === error.statusCode) {
+    send(res, 500, 'Cannot GET "/"');
+    // }
+  }
+}
+
+export default main;
